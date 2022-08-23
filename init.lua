3847c366e526cade85d5bfacfdef300a263fb931
Amogh Rathore <amoghr@amazon.com>
2022-08-23 00:26:21 +0000

    Add support for automatic file header for go files. Add back 'qf' to close quickfix map.

--- init.lua
+++ init.lua
@@ -39,6 +39,42 @@ if has('nvim') && executable('nvr')
 endif
 ]])
 
+-- 'q' to quit quickfix
+vim.api.nvim_create_autocmd(
+	"FileType",
+	{ pattern = { "qf" }, command = [[nnoremap <buffer><silent> q :close<CR>]] }
+)
+
+-- Check if file exists
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  local lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
+end
+
+-- Add Go header
+function add_go_header (t)
+	file = [[/home/amoghr/.config/nvim/headers/header.go]]
+	local lines = lines_from(file)
+	vim.api.nvim_buf_set_lines(t.buf, 0, 1, false, lines)
+end
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+	pattern = "*.go",
+	callback = add_go_header,
+})
+
 -- Setup plugins
 require('plugins') -- packer
 require('lsp') -- Setup LSP
