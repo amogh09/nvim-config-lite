041c8bc56c1a54fdb1c8e3f10ffb89f37d9897e9
Amogh Rathore <amoghdroid09@gmail.com>
2022-11-20 13:53:40 -0800

    Enable treesitter highlighting

--- init.lua
+++ init.lua
@@ -101,7 +101,15 @@ require('lsp') -- Setup LSP
 
 -- Treesitter
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"go"}
+    ensure_installed = {"go"},
+    highlight = {
+      enable = true,
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
+    },
 }
 
 -- nvim-test plugin
