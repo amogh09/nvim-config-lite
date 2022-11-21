1aee405ef72f0ed580749f823bce9b4e86829f93
Amogh Rathore <amoghr@amazon.com>
2022-11-21 14:04:46 -0800

    Update cmp usage as per new version

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,9 +1,14 @@
 -- LSP Setup
 local on_attach = require("lsp.onattach")
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+require'cmp'.setup {
+  sources = {
+    { name = 'nvim_lsp' }
+  }
+}
+
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Load lspconfig plugin
 local lspconfig = require('lspconfig')
