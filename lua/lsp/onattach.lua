395b5ee871a5fad72b9841986cc85623a259c695
Amogh Rathore <amogh09@gmail.com>
2023-10-05 19:49:01 +0000

    Add codelens support to lsp

--- lua/lsp/onattach.lua
+++ lua/lsp/onattach.lua
@@ -22,6 +22,8 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
 	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
 	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', '<space>cr', vim.lsp.codelens.refresh, bufopts)
+	vim.keymap.set('n', '<space>ce', vim.lsp.codelens.run, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', '<space>f', function()
 		vim.lsp.buf.format { async = true }
