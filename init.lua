4b9521557b66aed265f5310dcd183f58822b7e7b
Amogh Rathore <amogh09@gmail.com>
2023-10-05 19:49:56 +0000

    Reformat and Go header filepath update

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Settings
 vim.opt.number = true
 vim.opt.encoding = "utf-8"
-vim.opt.tabstop=4
-vim.opt.shiftwidth=4
-vim.opt.colorcolumn="100" -- Marker at 100 col width
-vim.opt.updatetime=200 -- Decrease update time to make vim gutter update faster
-vim.opt.foldenable=false -- Don't fold by default
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.colorcolumn = "100"               -- Marker at 100 col width
+vim.opt.updatetime = 200                  -- Decrease update time to make vim gutter update faster
+vim.opt.foldenable = false                -- Don't fold by default
 
-vim.opt.spell=false -- Enable built-in spell-checker
-vim.cmd[[au TermOpen * setlocal nospell]] -- Disable spell-checker in terminal mode
+vim.opt.spell = false                     -- Enable built-in spell-checker
+vim.cmd [[au TermOpen * setlocal nospell]] -- Disable spell-checker in terminal mode
 
 -- Newline in normal mode
 vim.keymap.set('n', '<Leader>o', 'o<Esc>')
@@ -22,10 +22,10 @@ vim.keymap.set('n', '<M-k>', '<c-w>k')
 vim.keymap.set('n', '<M-j>', '<c-w>j')
 vim.keymap.set('n', '<M-h>', '<c-w>h')
 
-vim.keymap.set('n', '<C-p>', ':<C-u>FZF<CR>') -- CTRL-p to trigger fzf
+vim.keymap.set('n', '<C-p>', ':<C-u>FZF<CR>')                -- CTRL-p to trigger fzf
 vim.keymap.set('n', 'cq', ':cclose<cr>', { noremap = true }) -- Close quickfix list
 
-vim.keymap.set('t', '<leader><c-r>', '<c-\\><c-n>pa') -- Paste in terminal mode
+vim.keymap.set('t', '<leader><c-r>', '<c-\\><c-n>pa')        -- Paste in terminal mode
 
 -- Move around windows in terminal mode
 vim.keymap.set('t', '<M-l>', '<c-\\><c-n><c-w>l')
@@ -61,60 +61,61 @@ endif
 
 -- 'q' to quit quickfix
 vim.api.nvim_create_autocmd(
-	"FileType",
-	{ pattern = { "qf" }, command = [[nnoremap <buffer><silent> q :close<CR>]] }
+    "FileType",
+    { pattern = { "qf" }, command = [[nnoremap <buffer><silent> q :close<CR>]] }
 )
 
 -- Remove trailing whitespace on save
-vim.api.nvim_create_autocmd( "BufWritePre", {
-	pattern = { "*.py", "*.lua", "*.zshrc" },
-	command = [[ :%s/\s\+$//e ]],
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = { "*.py", "*.lua", "*.zshrc" },
+    command = [[ :%s/\s\+$//e ]],
 })
 
 -- Check if file exists
 function file_exists(file)
-  local f = io.open(file, "rb")
-  if f then f:close() end
-  return f ~= nil
+    local f = io.open(file, "rb")
+    if f then f:close() end
+    return f ~= nil
 end
 
 -- get all lines from a file, returns an empty
 -- list/table if the file does not exist
 function lines_from(file)
-  if not file_exists(file) then return {} end
-  local lines = {}
-  for line in io.lines(file) do
-    lines[#lines + 1] = line
-  end
-  return lines
+    if not file_exists(file) then return {} end
+    local lines = {}
+    for line in io.lines(file) do
+        lines[#lines + 1] = line
+    end
+    return lines
 end
 
 -- Add Go header
-function add_go_header (t)
-	file = [[/home/amoghr/.config/nvim/headers/header.go]]
-	local lines = lines_from(file)
-	vim.api.nvim_buf_set_lines(t.buf, 0, 1, false, lines)
+function add_go_header(t)
+    home = os.getenv("HOME")
+    file = home .. "/.config/nvim/headers/header.go"
+    local lines = lines_from(file)
+    vim.api.nvim_buf_set_lines(t.buf, 0, 1, false, lines)
 end
 
 vim.api.nvim_create_autocmd("BufNewFile", {
-	pattern = "*.go",
-	callback = add_go_header,
+    pattern = "*.go",
+    callback = add_go_header,
 })
 
 -- Setup plugins
 require('plugins') -- packer
-require('lsp') -- Setup LSP
+require('lsp')     -- Setup LSP
 
 -- Treesitter
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"go", "lua", "json"},
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "go", "lua", "json" },
     highlight = {
-      enable = true,
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
     },
 }
 
@@ -134,4 +135,3 @@ vim.wo.foldenable = false
 
 -- nvim-test plugin
 require('setup_nvim_test')
-
