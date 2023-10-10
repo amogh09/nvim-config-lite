a2caa365f27d191f112e97028c2e7d406726f140
Amogh Rathore <amoghr@amazon.com>
2023-10-10 20:23:01 +0000

    Add delete_buffer mapping and editorconfig

--- init.lua
+++ init.lua
@@ -25,19 +25,29 @@ vim.keymap.set('n', '<M-h>', '<c-w>h')
 -- Telescope setup
 local success, telescope = pcall(require, "telescope.builtin")
 if success then
-	vim.keymap.set('n', '<space>p', telescope.find_files, {})
-	vim.keymap.set('n', '<space>g', telescope.live_grep, {})
-	vim.keymap.set('n', '<space>b', telescope.buffers, {})
-	vim.keymap.set('n', '<space>h', telescope.help_tags, {})
-	vim.keymap.set('n', '<space>s', telescope.lsp_dynamic_workspace_symbols, {})
-	vim.keymap.set('n', '<space>y', telescope.lsp_document_symbols, {})
-	vim.keymap.set('n', '<space>ic', telescope.lsp_incoming_calls, {})
-	vim.keymap.set('n', '<space>oc', telescope.lsp_outgoing_calls, {})
+  vim.keymap.set('n', '<space>p', telescope.find_files, {})
+  vim.keymap.set('n', '<space>g', telescope.live_grep, {})
+  vim.keymap.set('n', '<space>b', telescope.buffers, {})
+  vim.keymap.set('n', '<space>h', telescope.help_tags, {})
+  vim.keymap.set('n', '<space>s', telescope.lsp_dynamic_workspace_symbols, {})
+  vim.keymap.set('n', '<space>y', telescope.lsp_document_symbols, {})
+  vim.keymap.set('n', '<space>ic', telescope.lsp_incoming_calls, {})
+  vim.keymap.set('n', '<space>oc', telescope.lsp_outgoing_calls, {})
+
+  require('telescope').setup {
+    defaults = {
+      mappings = {
+        n = {
+          ["<C-b>"] = "delete_buffer"
+        }
+      },
+    },
+  }
 end
 
 vim.keymap.set('n', 'cq', ':cclose<cr>', { noremap = true }) -- Close quickfix list
 
-vim.keymap.set('t', '<c-\\><c-r>', '<c-\\><c-n>pa')        -- Paste in terminal mode
+vim.keymap.set('t', '<c-\\><c-r>', '<c-\\><c-n>pa')          -- Paste in terminal mode
 
 -- Move around windows in terminal mode
 vim.keymap.set('t', '<M-l>', '<c-\\><c-n><c-w>l')
@@ -73,45 +83,45 @@ endif
 
 -- 'q' to quit quickfix
 vim.api.nvim_create_autocmd(
-    "FileType",
-    { pattern = { "qf" }, command = [[nnoremap <buffer><silent> q :close<CR>]] }
+  "FileType",
+  { pattern = { "qf" }, command = [[nnoremap <buffer><silent> q :close<CR>]] }
 )
 
 -- Remove trailing whitespace on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.py", "*.lua", "*.zshrc" },
-    command = [[ :%s/\s\+$//e ]],
+  pattern = { "*.py", "*.lua", "*.zshrc" },
+  command = [[ :%s/\s\+$//e ]],
 })
 
 -- Check if file exists
 local function file_exists(file)
-    local f = io.open(file, "rb")
-    if f then f:close() end
-    return f ~= nil
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
 end
 
 -- get all lines from a file, returns an empty
 -- list/table if the file does not exist
 local function lines_from(file)
-    if not file_exists(file) then return {} end
-    local lines = {}
-    for line in io.lines(file) do
-        lines[#lines + 1] = line
-    end
-    return lines
+  if not file_exists(file) then return {} end
+  local lines = {}
+  for line in io.lines(file) do
+    lines[#lines + 1] = line
+  end
+  return lines
 end
 
 -- Add Go header
 local function add_go_header(t)
-    local home = os.getenv("HOME")
-    local file = home .. "/.config/nvim/headers/header.go"
-    local lines = lines_from(file)
-    vim.api.nvim_buf_set_lines(t.buf, 0, 1, false, lines)
+  local home = os.getenv("HOME")
+  local file = home .. "/.config/nvim/headers/header.go"
+  local lines = lines_from(file)
+  vim.api.nvim_buf_set_lines(t.buf, 0, 1, false, lines)
 end
 
 vim.api.nvim_create_autocmd("BufNewFile", {
-    pattern = "*.go",
-    callback = add_go_header,
+  pattern = "*.go",
+  callback = add_go_header,
 })
 
 -- Setup plugins
@@ -120,15 +130,15 @@ require('lsp')     -- Setup LSP
 
 -- Treesitter
 require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "go", "lua", "json" },
-    highlight = {
-        enable = true,
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
+  ensure_installed = { "go", "lua", "json" },
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
 }
 
 -- Taboo
